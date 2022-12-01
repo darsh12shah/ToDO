0c4d17e32184f3e35b30d9c1f1de3c53e7a92f21
darsh12shah <73706224+darsh12shah@users.noreply.github.com>
2022-12-01 14:10:28 +0530

    Update account.js

--- task-manager-api-main/task-manager/src/emails/account.js
+++ task-manager-api-main/task-manager/src/emails/account.js
@@ -9,7 +9,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'anunaynalam@gmail.com',
+        from: 'darsh12shah@gmail.com',
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
